2c158a2efd3b388b3db7cd3d4f081d805c7d69dc
Arkadiusz Lewicki <43863162+SqrtLewy@users.noreply.github.com>
2019-09-28 09:42:50 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<h2>Insert grade for test:</h2>
+<form action="index.php" method="POST">
+Name:<input type="text" name="name"> <br />
+Grade (1-6): <input type="number" name="grade"> <br />
+<input type="submit" value="Insert">
+</form>
+
+<h2>Grades:</h2>
+
+
+<?php
+
+$name = $_POST['name'];
+$grade = $_POST['grade'];
+
+$grades = array('John' => '4', 'Jack' => '5', 'Daniel' => '6', 'Paul'  => '2', "$name" => "$grade");
+
+ksort($grades);
+
+foreach ($grades as $key => $value)
+    echo $key." received a grade ".$value."<br />";
+
+?>
